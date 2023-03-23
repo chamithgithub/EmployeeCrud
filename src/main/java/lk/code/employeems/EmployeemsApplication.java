cab48b0bde91894fead0c970b9f50864dceafab7
Cha1552164 <chamithgithub>
2023-03-23 21:27:53 +0530

    create employeeCrud project

---
+++ src/main/java/lk/code/employeems/EmployeemsApplication.java
@@ -0,0 +1,13 @@
+package lk.code.employeems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemsApplication.class, args);
+	}
+
+}
