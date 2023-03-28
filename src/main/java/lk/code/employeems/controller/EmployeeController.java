db20b646bd8f4f12e25fd78194ca8420910d9ce3
Cha1552164 <chamithgithub>
2023-03-28 09:38:01 +0530

    updateEmployee controller implement

--- src/main/java/lk/code/employeems/controller/EmployeeController.java
+++ src/main/java/lk/code/employeems/controller/EmployeeController.java
@@ -47,4 +47,33 @@ public class EmployeeController {
             return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        try {
+            String res = employeeService.updateEmployee(employeeDTO);
+            if (res.equals("00")) {
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+            } else if (res.equals("01")) {
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("Not A Registered Employee..!!");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            } else {
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception exception) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(exception.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
