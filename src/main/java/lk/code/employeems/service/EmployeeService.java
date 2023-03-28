46c5375c9e726797714e0a0b06e31acf361448b6
Cha1552164 <chamithgithub>
2023-03-28 09:51:18 +0530

    EmployeeService  implement

--- src/main/java/lk/code/employeems/service/EmployeeService.java
+++ src/main/java/lk/code/employeems/service/EmployeeService.java
@@ -5,10 +5,13 @@ import lk.code.employeems.entity.Employee;
 import lk.code.employeems.repo.EmployeeRepo;
 import lk.code.employeems.util.VarList;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -37,6 +40,11 @@ public class EmployeeService {
         } else {
             return VarList.RSP_NO_DATA_FOUND;
         }
+    }
+    public List<EmployeeDTO>getAllEmplokyee() {
+        List<Employee> employeeList = employeeRepo.findAll();
+        return modelMapper.map(employeeList, new TypeToken<ArrayList<EmployeeDTO>>() {
+        }.getType());
     }
 
 }
